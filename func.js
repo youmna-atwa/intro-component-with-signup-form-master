531246a817e6231d813c49a60a1b42d018a4ede8
Mohamed Fathy <mohamedfathy234@gmail.com>
2023-11-13 22:39:25 +0200

    intiail

--- func.js
+++ func.js
@@ -1,27 +1,37 @@
-function myFunction() {
-    let x = document.getElementById("mustfirstName").value;
-    let y = document.getElementById("mustLastName").value;
-    // let z = document.getElementById("mustEmail").value;
-    // let A = document.getElementById("mustPass").value;
-    let text , text2;
+function myFunction(event) {
+
+    let firstName = document.getElementById("mustfirstName").value;
+    let lastName = document.getElementById("mustLastName").value;
     
+    document.getElementById('valid').innerHTML = '';
+    document.getElementById('valid2').innerHTML = '';
 
+    //Validtating regex
     var  regex = /^[a-zA-Z]+$/;
-    if (!x.match(regex) ) {
-        text = "Unvalid! FirstName Must Be String";
-        document.getElementById("valid").innerHTML = text;
-
-    } else {
-        document.getElementById('valid').innerHTML = '';
-    }
-    if (!y.match(regex)){
-        text2 = "Unvalid! LastName Must Be String";
-        document.getElementById("valid2").innerHTML = text2;
+    if (!firstName.match(regex))
+    { 
+        document.getElementById("valid").innerHTML = "Invalid! FirstName Must Be String";
+        event.preventDefault();
 
     }
-    else {
-        document.getElementById('valid2').innerHTML = '';
+    
+    if (!lastName.match(regex))
+    {
+        document.getElementById("valid2").innerHTML = "Invalid! LastName Must Be String";
+        event.preventDefault();
+
     }
 
-   
-  }
+  //  event.preventDefault();
+
+}
+
+  function StringOnlyFunc(evt) 
+  {
+	//evt = (evt) ? evt : window.event;
+	var charCode = (evt.which) ? evt.which : evt.keyCode;
+	if ( charCode > 48 && charCode < 57)
+		return false;
+
+	return true;
+}
